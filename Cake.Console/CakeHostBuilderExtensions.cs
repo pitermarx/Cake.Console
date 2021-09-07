bc1ac699d16d9a8d4ee6264508b754c26aec1e7c
ριтєя мαяχ <pedrohmarques@gmail.com>
2021-09-07 11:30:51 +0100

    cake cli integratino

--- Cake.Console/CakeHostBuilderExtensions.cs
+++ Cake.Console/CakeHostBuilderExtensions.cs
@@ -1,55 +1,64 @@
 using System.Threading.Tasks;
+using Cake.Cli;
 using Cake.Common;
 using Cake.Common.Diagnostics;
 using Cake.Console.HostBuilderBehaviours;
+using Cake.Core;
 using Cake.Core.Composition;
+using Cake.Core.Diagnostics;
+using Cake.Core.Scripting;
 
 namespace Cake.Console
 {
     public static class CakeHostBuilderExtensions
     {
-        public static CakeHostBuilder InstallNugetTool(this CakeHostBuilder builder, string id, string version)
-        {
-            builder.ConfigureServices(s => s.RegisterInstance(new CakeNugetTool(id, version)).As<ICakeToolReference>());
-            return builder;
-        }
+        public static CakeHostBuilder InstallNugetTool(this CakeHostBuilder builder, string id, string version) => builder
+            .ConfigureSingleton<CakeNugetTool, ICakeToolReference>(new CakeNugetTool(id, version));
 
-        public static CakeHostBuilder RegisterTasks<T>(this CakeHostBuilder builder) where T : ICakeTasks
-        {
-            builder.ConfigureServices(s => s.RegisterType<T>().As<ICakeTasks>().Singleton());
-            return builder;
-        }
+        public static CakeHostBuilder RegisterTasks<T>(this CakeHostBuilder builder) where T : class, ICakeTasks => builder
+            .ConfigureSingleton<T, ICakeTasks>();
 
-        public static CakeHostBuilder WorkingDirectory<T>(this CakeHostBuilder builder) where T : IWorkingDirectory
-        {
-            builder.ConfigureServices(s => s.RegisterType<T>().As<IWorkingDirectory>().Singleton());
-            return builder;
-        }
+        public static CakeHostBuilder WorkingDirectory<T>(this CakeHostBuilder builder) where T : class, IWorkingDirectory => builder
+            .ConfigureSingleton<T, IWorkingDirectory>();
 
-        public static CakeHostBuilder ContextData<T>(this CakeHostBuilder builder) where T : class
-        {
-            builder.ConfigureServices(s => s.RegisterType<T>().AsSelf().Singleton());
-            builder.ConfigureServices(s => s.RegisterType<SetupContextDataBehaviour<T>>().As<IHostBuilderBehaviour>().Singleton());
-            return builder;
-        }
+        public static CakeHostBuilder ContextData<T>(this CakeHostBuilder builder, T instance = null) where T : class => builder
+            .ConfigureSingleton<T, T>(instance)
+            .ConfigureSingleton<SetupContextDataBehaviour<T>, IHostBuilderBehaviour>();
 
-        public static CakeHostBuilder ContextData<T>(this CakeHostBuilder builder, T instance) where T : class
-        {
-            builder.ConfigureServices(s => s.RegisterInstance(instance).AsSelf().Singleton());
-            builder.ConfigureServices(s => s.RegisterType<SetupContextDataBehaviour<T>>().As<IHostBuilderBehaviour>().Singleton());
-            return builder;
-        }
-
-        public static Task Run(this CakeHostBuilder builder, string defaultTarget = null)
-        {
-            var host = builder.Build();
-            if (host.Context.Argument("Target", defaultTarget) is string t)
+        private static CakeHostBuilder ConfigureSingleton<TImpl, T>(this CakeHostBuilder builder, TImpl instance = null) where TImpl : class => builder
+            .ConfigureServices(s =>
             {
-                return host.RunTargetAsync(t);
+                var registration = instance is null
+                    ? s.RegisterType<TImpl>()
+                    : s.RegisterInstance(instance);
+                registration.As<T>().Singleton();
+            });
+
+        public static Task RunCakeCli(this CakeHostBuilder builder, string target = null)
+            => builder.Build().RunCakeCli(target);
+
+        public static Task RunCakeCli(this IScriptHost host, string target = null)
+        {
+            if (host.Context.HasArgument("version"))
+            {
+                new VersionFeature(new VersionResolver()).Run(new CakeConsole(host.Context.Environment));
+                return Task.CompletedTask;
             }
 
-            host.Context.Error("No target specified");
-            return Task.CompletedTask;
+            if (host.Context.HasArgument("info"))
+            {
+                new InfoFeature(new VersionResolver()).Run(new CakeConsole(host.Context.Environment));
+                return Task.CompletedTask;
+            }
+
+            target ??= host.Context.Argument<string>("target");
+            if (target is null)
+            {
+                host.Context.Error("No target specified");
+                return Task.CompletedTask;
+            }
+
+            return host.RunTargetAsync(target);
         }
     }
 }
